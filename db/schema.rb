af6c97af26941f05bced3ef6f188805e97bfe0ac
Enrique Vidal <enrique@cloverinteractive.com>
2013-08-03 20:04:15 -0700

    Reset schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120918223422) do
+ActiveRecord::Schema.define(:version => 20120918223422) do
 
-  create_table "assets", force: true do |t|
+  create_table "assets", :force => true do |t|
     t.string  "description"
     t.string  "asset_file_name"
     t.string  "asset_content_type"
@@ -22,45 +22,45 @@ ActiveRecord::Schema.define(version: 20120918223422) do
     t.string  "attachable_type"
   end
 
-  add_index "assets", ["asset_content_type"], name: "index_assets_on_asset_content_type"
-  add_index "assets", ["attachable_id", "attachable_type"], name: "index_assets_on_attachable_id_and_attachable_type"
+  add_index "assets", ["asset_content_type"], :name => "index_assets_on_asset_content_type"
+  add_index "assets", ["attachable_id", "attachable_type"], :name => "index_assets_on_attachable_id_and_attachable_type"
 
-  create_table "pages", force: true do |t|
+  create_table "pages", :force => true do |t|
     t.string   "name"
     t.text     "content"
     t.integer  "section_id"
-    t.boolean  "published",   default: false
-    t.boolean  "home_page",   default: false
-    t.boolean  "has_contact", default: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.boolean  "published",   :default => false
+    t.boolean  "home_page",   :default => false
+    t.boolean  "has_contact", :default => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.string   "slug"
   end
 
-  add_index "pages", ["section_id"], name: "index_pages_on_section_id"
-  add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true
+  add_index "pages", ["section_id"], :name => "index_pages_on_section_id"
+  add_index "pages", ["slug"], :name => "index_pages_on_slug", :unique => true
 
-  create_table "sections", force: true do |t|
+  create_table "sections", :force => true do |t|
     t.string   "name"
     t.integer  "main_section_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "slug"
   end
 
-  add_index "sections", ["main_section_id"], name: "index_sections_on_main_section_id"
-  add_index "sections", ["slug"], name: "index_sections_on_slug", unique: true
+  add_index "sections", ["main_section_id"], :name => "index_sections_on_main_section_id"
+  add_index "sections", ["slug"], :name => "index_sections_on_slug", :unique => true
 
-  create_table "settings", force: true do |t|
-    t.string   "name",                          null: false
-    t.text     "value",                         null: false
+  create_table "settings", :force => true do |t|
+    t.string   "name",                          :null => false
+    t.text     "value",                         :null => false
     t.string   "description"
-    t.boolean  "destroyable", default: true
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.boolean  "destroyable", :default => true
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
-  create_table "taggings", force: true do |t|
+  create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
@@ -70,22 +70,22 @@ ActiveRecord::Schema.define(version: 20120918223422) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tagger_id", "tagger_type"], :name => "index_taggings_on_tagger_id_and_tagger_type"
 
-  create_table "tags", force: true do |t|
+  create_table "tags", :force => true do |t|
     t.string "name"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "username",               default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "username",               :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -94,17 +94,17 @@ ActiveRecord::Schema.define(version: 20120918223422) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0
+    t.integer  "failed_attempts",        :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
