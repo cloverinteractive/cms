4838220b8bcce3b28044ace9a138f80d81b2e6e6
Enrique Vidal <enrique@cloverinteractive.com>
2013-10-04 19:42:45 -0700

    Update bootstrap_pagedown to include pagedown-extra

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,5 +1,13 @@
 $(function() {
   $.fn.loopedSlider.defaults.addPagination = true;
   $('#slider').loopedSlider({ autoStart: 5000, containerClick: false });
+
+  if ( $("#wmd-input").length > 0 ) {
+    var converter = Markdown.getSanitizingConverter();
+    var editor    = new Markdown.Editor( converter );
+
+    Markdown.Extra.init( converter );
+    editor.run();
+  }
 });
 
